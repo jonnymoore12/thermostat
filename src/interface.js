6b22da530f998872426729e6889acb538047a66b
Jonny Moore <jonnymoore121212@gmail.com>
2016-08-04 16:28:11 +0100

    Colours display

--- src/interface.js
+++ src/interface.js
@@ -1,26 +1,50 @@
-var thermostat = new Thermostat();
+$(document).ready(function(){
+  var thermostat = new Thermostat();
+  $('#Thermostat-getTemperature').text(thermostat.getTemperature());
 
-$( "#Thermostat-up" ).click(function(){
-  thermostat.up();
-  $( "#Thermostat-getTemperature" ).text(thermostat.getTemperature());
-});
+  $( "#Thermostat-up" ).click(function(){
+    thermostat.up();
+    // $( "#Thermostat-getTemperature" ).text(thermostat.getTemperature()).css("color", function(){
+    //   if(thermostat.getEnergyLevel(thermostat.getTemperature()) === "Low-Energy"){
+    //     return "green";
+    //   } else if (thermostat.getEnergyLevel(thermostat.getTemperature()) === "Medium-Energy"){
+    //     return "yellow";
+    //   } else {
+    //   return "red";
+    //   };
+    // });
+  });
 
-$( "#Thermostat-down" ).click(function(){
-  thermostat.down();
-  $( "#Thermostat-getTemperature" ).text(thermostat.getTemperature());
-});
+  $( "#Thermostat-down" ).click(function(){
+    thermostat.down();
+    // $( "#Thermostat-getTemperature" ).text(thermostat.getTemperature());
+  });
 
-$( "#Thermostat-resetTemp" ).click(function(){
-  thermostat.resetTemp();
-  $( "#Thermostat-getTemperature" ).text(thermostat.getTemperature());
-});
+  $( "#Thermostat-resetTemp" ).click(function(){
+    thermostat.resetTemp();
+    // $( "#Thermostat-getTemperature" ).text(thermostat.getTemperature());
+  });
 
-$( "#Thermostat-powerModeSwitch" ).click(function(){
-  thermostat.powerModeSwitch();
-  $( "#Thermostat-getTemperature" ).text(thermostat.getTemperature());
-  $( "#Thermostat-isPowerSavingOn" ).text(function() { if(thermostat.isPowerSavingOn()) {
-     return "on";
-    } else {
-     return "off";
+  $( "#Thermostat-powerModeSwitch" ).click(function(){
+    thermostat.powerModeSwitch();
+    // $( "#Thermostat-getTemperature" ).text(thermostat.getTemperature());
+    $( "#Thermostat-isPowerSavingOn" ).text(function() { if(thermostat.isPowerSavingOn()) {
+       return "on";
+      } else {
+       return "off";
     }});
   });
+
+  $("button").click(function(){
+    $( "#Thermostat-getTemperature" ).text(thermostat.getTemperature()).css("color", function(){
+      if(thermostat.getEnergyLevel(thermostat.getTemperature()) === "Low-Energy"){
+        return "green";
+      } else if (thermostat.getEnergyLevel(thermostat.getTemperature()) === "Medium-Energy"){
+        return "yellow";
+      } else {
+      return "red";
+      };
+    });
+  });
+
+});
