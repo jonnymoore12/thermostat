d1ba9e989c8a2f1a93e657456dfcd3537b55dcba
Jonny Moore <jonnymoore121212@gmail.com>
2016-08-04 20:23:08 +0100

    Refactors interface

--- src/interface.js
+++ src/interface.js
@@ -1,58 +1,69 @@
 $(document).ready(function(){
   var thermostat = new Thermostat();
-  $('#Thermostat-getTemperature').text(thermostat.getTemperature());
+  $('#getTemperature').text(thermostat.getTemperature());
 
-  $( "#Thermostat-up" ).click(function(){
+  $( "#up" ).click(function(){
     thermostat.up();
-    // $( "#Thermostat-getTemperature" ).text(thermostat.getTemperature()).css("color", function(){
-    //   if(thermostat.getEnergyLevel(thermostat.getTemperature()) === "Low-Energy"){
-    //     return "green";
-    //   } else if (thermostat.getEnergyLevel(thermostat.getTemperature()) === "Medium-Energy"){
-    //     return "yellow";
-    //   } else {
-    //   return "red";
-    //   };
-    // });
+    updateTemperature();
   });
 
-  $( "#Thermostat-down" ).click(function(){
+  $( "#down" ).click(function(){
     thermostat.down();
-    // $( "#Thermostat-getTemperature" ).text(thermostat.getTemperature());
+    updateTemperature();
   });
 
-  $( "#Thermostat-resetTemp" ).click(function(){
+  $( "#resetTemp" ).click(function(){
     thermostat.resetTemp();
-    // $( "#Thermostat-getTemperature" ).text(thermostat.getTemperature());
+    updateTemperature();
   });
 
-  $( "#Thermostat-powerModeSwitch" ).click(function(){
+  $( "#powerModeSwitch" ).click(function(){
     thermostat.powerModeSwitch();
-    // $( "#Thermostat-getTemperature" ).text(thermostat.getTemperature());
-    $( "#Thermostat-isPowerSavingOn" ).text(function() { if(thermostat.isPowerSavingOn()) {
+    $( "#isPowerSavingOn" ).text(function() { if(thermostat.isPowerSavingOn()) {
        return "on";
       } else {
        return "off";
     }});
   });
 
-  $("button").click(function(){
-    $( "#Thermostat-getTemperature" ).text(thermostat.getTemperature()).css("color", function(){
-      if(thermostat.getEnergyLevel(thermostat.getTemperature()) === "Low-Energy"){
-        return "green";
-      } else if (thermostat.getEnergyLevel(thermostat.getTemperature()) === "Medium-Energy"){
-        return "yellow";
-      } else {
-      return "red";
-      };
-    });
-  });
-
-  $.ajax({
-    url: "http://api.wunderground.com/api/fb3946a1b0abcf37/conditions/q/UK/London.json",
-    dataType: 'json',
-    success: function(data){
-      alert( "The curret temperature in Londin is: " + data.current_observation.temp_c );
+  function updateTemperature(){
+    $('#getTemperature').text(thermostat.getTemperature());
+    if(thermostat.getEnergyLevel(thermostat.getTemperature()) === 'Low-Energy') {
+      $('#getTemperature').css('color', 'green')
+    } else if(thermostat.getEnergyLevel(thermostat.getTemperature()) === 'Medium-Energy') {
+      $('#getTemperature').css('color', 'yellow')
+    } else {
+      $('#getTemperature').css('color', 'red')
     }
-  });
+  }
 
 });
+
+
+  // $("button").click(function(){
+  //   $( "#getTemperature" ).text(thermostat.getTemperature()).css("color", function(){
+  //     if(thermostat.getEnergyLevel(thermostat.getTemperature()) === "Low-Energy"){
+  //       return "green";
+  //     } else if (thermostat.getEnergyLevel(thermostat.getTemperature()) === "Medium-Energy"){
+  //       return "yellow";
+  //     } else {
+  //     return "red";
+  //     };
+  //   });
+  // });
+
+
+
+
+
+
+
+
+
+  // $.ajax({
+  //   url: "http://api.wunderground.com/api/7c62def172936516/conditions/q/UK/London.json",
+  //   dataType: 'json',
+  //   success: function(data){
+  //     alert( "The current temperature in London is: " + data.current_observation.temp_c );
+  //   }
+  // });
